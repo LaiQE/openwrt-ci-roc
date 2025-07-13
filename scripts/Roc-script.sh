b6873af4d8d13a101cc17f2d935f41803d43f3ab
laiqe <laiqe@qq.com>
2025-07-13 15:41:09 +0800

    移除Git配置相关代码，更新Roc脚本以使用HTTPS进行稀疏克隆

--- scripts/Roc-script.sh
+++ scripts/Roc-script.sh
@@ -1,6 +1,3 @@
-# 配置 Git
-git config --global --unset credential.helper
-git config --global url."http://github.com/".insteadOf "git@github.com:"
 # 修改默认IP & 固件名称 & 编译署名
 sed -i 's/192.168.1.1/192.168.233.1/g' package/base-files/files/bin/config_generate
 sed -i "s/hostname='.*'/hostname='Arthur'/g" package/base-files/files/bin/config_generate
@@ -16,9 +13,6 @@ sed -i 's|/bin/login|/bin/login -f root|g' feeds/packages/utils/ttyd/files/ttyd.
 mkdir -p feeds/packages/net/vlmcsd/patches
 cp -f $GITHUB_WORKSPACE/patches/fix_vlmcsd_compile_with_ccache.patch feeds/packages/net/vlmcsd/patches
 
-# 开启NSS全部功能
-cp -f $GITHUB_WORKSPACE/patches/Makefile target/linux/qualcommax/Makefile
-
 # 移除要替换的包
 rm -rf feeds/packages/net/open-app-filter
 rm -rf feeds/packages/net/adguardhome
@@ -28,23 +22,23 @@ rm -rf feeds/luci/applications/luci-app-openlist
 # Git稀疏克隆，只克隆指定目录到本地
 function git_sparse_clone() {
   branch="$1" repourl="$2" && shift 2
-  # 确保 URL 是匿名访问形式
   git clone --depth=1 -b $branch --single-branch --filter=blob:none --sparse $repourl
   repodir=$(echo $repourl | awk -F '/' '{print $(NF)}')
   cd $repodir && git sparse-checkout set $@
   mv -f $@ ../package
   cd .. && rm -rf $repodir
 }
+
 # Go & OpenList & AdGuardHome & AriaNg & WolPlus & Lucky & OpenAppFilter & 集客无线AC控制器 & 雅典娜LED控制
-# git clone --depth=1 http://github.com/sbwml/packages_lang_golang feeds/packages/lang/golang
-# git clone --depth=1 http://github.com/sbwml/luci-app-openlist package/openlist
-git_sparse_clone master http://github.com/kenzok8/openwrt-packages adguardhome luci-app-adguardhome
-git_sparse_clone master http://github.com/laipeng668/packages ariang luci-app-wolplus
-# git clone --depth=1 http://github.com/gdy666/luci-app-lucky package/luci-app-lucky
-# git clone --depth=1 http://github.com/destan19/OpenAppFilter.git package/OpenAppFilter
-# git clone --depth=1 http://github.com/lwb1978/openwrt-gecoosac package/openwrt-gecoosac
-# git clone --depth=1 http://github.com/NONGFAH/luci-app-athena-led package/luci-app-athena-led
+# git clone --depth=1 https://github.com/sbwml/luci-app-openlist package/openlist
+git_sparse_clone master https://github.com/kenzok8/openwrt-packages adguardhome luci-app-adguardhome
+git_sparse_clone main https://github.com/VIKINGYFY/packages luci-app-wolplus
+# git clone --depth=1 https://github.com/gdy666/luci-app-lucky package/luci-app-lucky
+# git clone --depth=1 https://github.com/destan19/OpenAppFilter.git package/OpenAppFilter
+# git clone --depth=1 https://github.com/lwb1978/openwrt-gecoosac package/openwrt-gecoosac
+# git clone --depth=1 https://github.com/NONGFAH/luci-app-athena-led package/luci-app-athena-led
 # chmod +x package/luci-app-athena-led/root/etc/init.d/athena_led package/luci-app-athena-led/root/usr/sbin/athena-led
+
 
 # tailscale
 git clone --depth=1 http://github.com/asvow/luci-app-tailscale package/luci-app-tailscale
